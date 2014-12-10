7a8299498851c8fc658da8940188cdf533d5c7ec
inessler <iannessler@gmail.com>
2014-12-10 15:27:49 -0600

    update

--- public_html/js/libs/angular.js/angularController.js
+++ public_html/js/libs/angular.js/angularController.js
@@ -31,6 +31,10 @@ WebApp.controller('EnergyController', ['$scope', '$window', function ($scope) {
             } else {
                 alert('The File APIs are not fully supported in this browser.');
             }
+            if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+                alert('The File APIs are not fully supported in this browser.');
+                return;
+            }
             //Build Textstring to download
             var TextString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE jnlp PUBLIC '-//Sun Microsystems, Inc//DTD JNLP Descriptor 6.0//EN' 'http://java.sun.com/dtd/JNLP-6.0.dtd'>\n<jnlp spec=\"6.0+\">\n    <information>\n        <title>Force Field X</title>\n        <vendor>Michael J. Schnieders</vendor>\n        <homepage href=\"http://ffx.eng.uiowa.edu\"/>\n        <description>Software for Molecular Biophysics</description>\n        <icon href=\"images/icon128.png\"/>\n        <offline-allowed/>\n    </information>\n    <security>\n        <all-permissions/>\n    </security>\n    <update check=\"always\" policy=\"always\"/>\n    <resources>\n        <java version=\"1.8\" initial-heap-size=\"1G\" max-heap-size=\"1G\"/>\n        <property name=\"j3d.rend\" value=\"jogl\"/>\n        <extension name=\"ffx-all\" href=\"http://ffx.eng.uiowa.edu/ffx-commons/ffx-all-1.0.0-beta.jnlp\" />\n        <extension name=\"ffx-dependency\" href=\"http://ffx.eng.uiowa.edu/dependency-repo/ffx-dependency-1.0.0-beta.jnlp\" />\n        <extension name=\"jogl-all-awt\" href=\"http://jogamp.org/deployment/v2.1.5/jogl-all-awt.jnlp\" />\n    </resources>\n    <application-desc main-class=\"ffx.Main\">\n       <argument> minimize"
             if (Number0 !== undefined) {
@@ -67,41 +71,16 @@ WebApp.controller('EnergyController', ['$scope', '$window', function ($scope) {
             TextString = TextString + "Downloads/" + FileName + " </argument>\n    </application-desc>\n</jnlp>";
             //Textstring compile is completed create download element.
             $scope.JNLP = TextString;
-            $scope.data = new Blob([TextString], {type: 'text/plain'});
-            var data = new Blob([TextString], {type: 'text/plain'});
-//            saveAs(data,"ffx.jnlp");
-//The code below this block can be used if saveAs()
-//Does not work in the future, I think the save as is a better implementation as of this moment
-            var a = window.document.createElement('a');
-            a.download = 'ffx.jnlp';
-//            a.href = $scope.JNLP_Directory;
-            $scope.URL = window.URL.createObjectURL(data);
-            a.href = $scope.URL;
-//            uriContent = "data:application/octet-stream,"+encodeURIComponent($scope.JNLP);
-//            a.href = uriContent;
-            console.log(a.href);
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
             
-//            location.href=uriContent;
-//             doc.output($scope.URL);
-
-
-            //Start of dealing with input pdb file    
-            if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-                alert('The File APIs are not fully supported in this browser.');
-                return;
-            }
             var fr = new FileReader();
             fr.onload = receivedText;
             fr.readAsText(document.getElementById("PDB_File").files[0]);
-            function receivedText() {
+                function receivedText() {
 //               var result = document.createTextNode(fr.result);
                 var result2 = fr.result;
-                console.log(result2);
+                console.log('result2');
                 var data2 = new Blob([result2], {type: 'text/plain'});
-                console.log(data2);
+                console.log('data2');
                 console.log("please see this")
 
                 var FileName = document.getElementById("PDB_File").files[0].name;
@@ -117,13 +96,39 @@ WebApp.controller('EnergyController', ['$scope', '$window', function ($scope) {
                 document.body.appendChild(b);
                 b.click();
                 document.body.removeChild(b);
+                
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+                
                 //After download take user to FAQ page about what to do next.
                 var c = window.document.createElement('a');
                 c.href = "FileDownloaded.html";
                 document.body.appendChild(c);
-                c.click();
+//                c.click();
                 document.body.removeChild(c);
             }
+            
+            
+            $scope.data = new Blob([TextString], {type: 'text/plain'});
+            var data = new Blob([TextString], {type: 'text/plain'});
+//            saveAs(data,"ffx.jnlp");
+//The code below this block can be used if saveAs()
+//Does not work in the future, I think the save as is a better implementation as of this moment
+            var a = window.document.createElement('a');
+            a.download = 'ffx.jnlp';
+            $scope.URL = window.URL.createObjectURL(data);
+            a.href = $scope.URL;
+//            uriContent = "data:application/octet-stream,"+encodeURIComponent($scope.JNLP);
+//            a.href = uriContent;
+            console.log('last');
+            
+            
+//            location.href=uriContent;
+//             doc.output($scope.URL);
+
+
+            //Start of dealing with input pdb file    
         };
     }]);
 
@@ -148,6 +153,10 @@ WebApp.controller('Molecular_Dynamics_Controller', ['$scope', '$window', functio
                 // Great success! All the File APIs are supported.
             } else {
                 alert('The File APIs are not fully supported in this browser.');
+            }
+                        if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+                alert('The File APIs are not fully supported in this browser.');
+                return;
             }
 
 //          Build JNLP as Textstring to download
@@ -203,10 +212,6 @@ WebApp.controller('Molecular_Dynamics_Controller', ['$scope', '$window', functio
 
 
             //Start of dealing with input pdb file    
-            if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-                alert('The File APIs are not fully supported in this browser.');
-                return;
-            }
             var fr = new FileReader();
             fr.onload = receivedText;
             fr.readAsText(document.getElementById("PDB_File").files[0]);
