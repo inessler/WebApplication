1a94a233bbf60439b6ec9f251ea744e4e0fa57f4
Ian J. Nessler <ian-nessler@uiowa.edu>
2015-01-07 16:32:52 -0600

    Heavily detailed comments for angularController.js, index.html, and Main.css

--- public_html/js/libs/angular.js/angularController.js
+++ public_html/js/libs/angular.js/angularController.js
@@ -23,33 +23,66 @@ WebApp.controller('EnergyController', ['$scope', '$window', function ($scope) {
         $scope.Number3 = "";
         $scope.Number4 = "";
         $scope.Number5 = "";
+        
         var DownloadCheck = function () {
-            // create temporary hyperlink element
+
+            /*****************************************************************************
+             * create temporary hyperlink element
+             * @type @exp;document@call;createElement
+             *****************************************************************************/
             var hyperlink = document.createElement('a');
-            // if download property is undefined
-// browser doesn't support the feature
+
+            /*****************************************************************************
+             *      if download property is undefined
+             *      browser doesn't support the feature
+             *****************************************************************************/
             if (hyperlink.download === undefined) {
                 alert('Bummer... \nIt would appear your browser does not support download attributes.\n\nPlease switch to a compatible browser such as:\nGoogle Chrome\nOpera\nFirefox');
             }
         };
-        DownloadCheck();
         
-        //         Function to collect flags input then download JNLP file and input file
+        /*****************************************************************************
+         * Run DownloadCheck function to see if browser allows download attributes
+         * @returns {undefined}
+         *****************************************************************************/
+        DownloadCheck();
+
+        /*****************************************************************************
+         * Function to collect flags input then download JNLP file and input file
+         * @param {type} Number1
+         * @param {type} Number2
+         * @param {type} Number3
+         * @param {type} Number4
+         * @param {type} Number5
+         * @returns {undefined}
+         *****************************************************************************/
         $scope.CollectFlags = function (Number1, Number2, Number3, Number4, Number5) {
 
-            // Check for the various File API support. 
+            /*****************************************************************************
+             * Check for the various File API support. 
+             *****************************************************************************/ 
             if (window.File && window.FileReader && window.FileList && window.Blob) {
-                // Great success! All the File APIs are supported.
+                
+                /*****************************************************************************
+                 * Great success! All the File APIs are supported.
+                 *****************************************************************************/ 
             } else {
                 alert('The File APIs are not fully supported in this browser.');
                 return;
             }
-//          Build JNLP as Textstring to download
+            
+            /*****************************************************************************
+             * Build JNLP as Textstring to download
+             *****************************************************************************/ 
             var InitialTextString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE jnlp PUBLIC '-//Sun Microsystems, Inc//DTD JNLP Descriptor 6.0//EN' 'http://java.sun.com/dtd/JNLP-6.0.dtd'>\n<jnlp spec=\"6.0+\">\n    <information>\n        <title>Force Field X</title>\n        <vendor>Michael J. Schnieders</vendor>\n        <homepage href=\"http://ffx.eng.uiowa.edu\"/>\n        <description>Software for Molecular Biophysics</description>\n        <icon href=\"images/icon128.png\"/>\n        <offline-allowed/>\n    </information>\n    <security>\n        <all-permissions/>\n    </security>\n    <update check=\"always\" policy=\"always\"/>\n    <resources>\n        <java version=\"1.8\" initial-heap-size=\"1G\" max-heap-size=\"1G\"/>\n        <property name=\"j3d.rend\" value=\"jogl\"/>\n        <extension name=\"ffx-all\" href=\"http://ffx.eng.uiowa.edu/ffx-commons/ffx-all-1.0.0-beta.jnlp\" />\n        <extension name=\"ffx-dependency\" href=\"http://ffx.eng.uiowa.edu/dependency-repo/ffx-dependency-1.0.0-beta.jnlp\" />\n        <extension name=\"jogl-all-awt\" href=\"http://jogamp.org/deployment/v2.1.5/jogl-all-awt.jnlp\" />\n    </resources>\n    <application-desc main-class=\"ffx.Main\">\n       <argument>";
-            var TextString = InitialTextString+"minimize";
-//            Next 5 if statements check to see if user input has been provided
-//            in input box if so it assigns the appropriate flag and appends it to
-//            the JNLP file string called TextString.
+            var FFX_Function = "minimize";
+            var TextString = InitialTextString + FFX_Function;
+            
+            /*******************************************************************************
+             *      Next 5 if statements check to see if user input has been provided
+             *      in input box if so it assigns the appropriate flag and appends it to
+             *      the JNLP file string called TextString. 
+             *******************************************************************************/
             if (Number1 !== undefined) {
                 var TextString = TextString + ' ' + '-s ' + Number1;
             }
@@ -72,8 +105,12 @@ WebApp.controller('EnergyController', ['$scope', '$window', function ($scope) {
             ;
             TextString = TextString + " </argument>\n       <argument> ";
 
-            // Check if user inputed a file if not then deliver a warning to user
-            // and exit function.
+            /*****************************************************************************
+             * 
+             *           Check if user inputed a file if not then deliver a warning to user
+             *           and exit function.
+             *           
+             *****************************************************************************/
             if (document.getElementById("PDB_File").files.length !== 0) {
                 var FileName = document.getElementById("PDB_File").files[0].name;
             }
@@ -83,76 +120,101 @@ WebApp.controller('EnergyController', ['$scope', '$window', function ($scope) {
             }
             TextString = TextString + "Downloads/" + FileName + " </argument>\n    </application-desc>\n</jnlp>";
 
-            //Textstring compile is completed create download element.
+            /*****************************************************************************
+             *            Textstring compile is completed create download element.
+             *****************************************************************************/
             var data = new Blob([TextString], {type: 'text/plain'});
             saveAs(data, "ffx.jnlp");
+            
+            /*****************************************************************************           
+             * The code below this block can be used if saveAs()
+             * does not work in the future, I think the save as is a better 
+             * implementation as of this moment
+             *                      
+             *            var a = window.document.createElement('a');
+             *            a.download = 'ffx.jnlp';
+             *            a.href = $scope.JNLP_Directory;
+             *            a.href = window.URL.createObjectURL(data);
+             *            document.body.appendChild(a);
+             *            a.click();
+             *            document.body.removeChild(a);
+             *****************************************************************************/
 
-
-/*           The code below this block can be used if saveAs()
-*            Does not work in the future, I think the save as is a better implementation as of this moment
-*                      
-*            var a = window.document.createElement('a');
-*            a.download = 'ffx.jnlp';
-*            a.href = $scope.JNLP_Directory;
-*            a.href = window.URL.createObjectURL(data);
-*            document.body.appendChild(a);
-*            a.click();
-*            document.body.removeChild(a);
-*/
-
-            //Start of dealing with input pdb file    
+            /***************************************************************************** 
+             *               Start of dealing with input pdb file   
+             *****************************************************************************/
             var fr = new FileReader();
             fr.onload = receivedText;
             fr.readAsText(document.getElementById("PDB_File").files[0]);
-//            migrateHtml();
-            // Function to collect input file information
+
+            /*****************************************************************************
+             *      The following function reads the input file and then saves it in the
+             *      users Downloads folder
+             *****************************************************************************/
             function receivedText() {
 
-                // Set input file to a variable
+                /*****************************************************************************
+                 *  Set input file to a variable
+                 * @returns {Element}
+                 *****************************************************************************/
                 var fileInputContents = fr.result;
 
-                // Create blob of input file contents
+                /*****************************************************************************
+                 *  Create blob of input file contents
+                 * @type @new;Blob
+                 *****************************************************************************/
                 var blobFileInput = new Blob([fileInputContents], {type: 'text/plain'});
 
-                // Get file input name and set that as the name for the pdb file download.
-                // In future may want to make a random process to avoid duplicates.
+                /*****************************************************************************
+                 *       Get file input name and set that as the name for the pdb file download.
+                 *       In future may want to make a random process to avoid duplicates.
+                 * @type @exp;document@call;getElementById@arr;files@pro;name
+                 *****************************************************************************/
                 var FileName = document.getElementById("PDB_File").files[0].name;
                 saveAs(blobFileInput, FileName);
+
+                /*****************************************************************************
+                 *The function below will take the user to FileDownloaded.html after completion. 
+                 * @returns {Element}
+                 *****************************************************************************/
                 migrateHtml();
 
-                /* The code below this block can be used if saveAs()
-  * Does not work in the future, I think the save as is a better implementation at this moment*/
-//                
-//                a.download = FileName;
-//                a.href = window.URL.createObjectURL(fileInputContents);
-//                document.body.appendChild(a);
-//                a.click();
-//                document.body.removeChild(a);
-                
+                /*****************************************************************************
+                 *      The code below this block can be used if saveAs()
+                 *      does not work in the future, I think the save as is a better 
+                 *      implementation at this moment
+                 *                 
+                 *                 a.download = FileName;
+                 *                 a.href = window.URL.createObjectURL(fileInputContents);
+                 *                 document.body.appendChild(a);
+                 *                 a.click();
+                 *                 document.body.removeChild(a);
+                 *****************************************************************************/
+
             }
-            
+
         };
-        
+
     }]);
+
 /************************************************************************************
-             * The following code provides a way to go from the minimize html page to the fileDownloaded html page
-             * So that the end user will know how to run the file that was previously downloaded.
-             * @type @exp;window@pro;document@call;createElement
-             ************************************************************************************/
+ *      The following code provides a way to go from the minimize html page to the fileDownloaded html page
+ *      So that the end user will know how to run the file that was previously downloaded.
+ * @type @exp;window@pro;document@call;createElement
+ ************************************************************************************/
 var migrateHtml = function () {
-            var a = window.document.createElement('a');
-            a.href = "FileDownloaded.html";
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);            
-        };
+    var a = window.document.createElement('a');
+    a.href = "FileDownloaded.html";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+};
 
-
-
-
-
-
-
+/*
+ * Future FFX functions could be conducted using seperate controllers with similar code on the inside.
+ * What would need to change is the the flags associated with each Number from the input and the FFX_Function
+ * 
+ */
 
 WebApp.controller('Molecular_Dynamics_Controller', ['$scope', '$window', function ($scope) {
 
