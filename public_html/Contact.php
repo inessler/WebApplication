7b650c2fa0f3efecfc0d33cb36ecede722280f7a
Ian J. Nessler <ian-nessler@uiowa.edu>
2015-06-12 16:07:47 -0500

    Minor changes to place header and footer on contact page

--- public_html/Contact.php
+++ public_html/Contact.php
@@ -175,7 +175,7 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
                     </div>
                 </div>
             </div>
-            <div style="height: 10px"></div>
+            <div style="height: 50px"></div>
   	<div class="container">
   		<div class="row">
   			<div class="col-md-6 col-md-offset-3">
