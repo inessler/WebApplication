f6cf390e0edb66404fa2f789e19b8eaf32c2983b
Ian J. Nessler <ian-nessler@uiowa.edu>
2015-06-16 08:30:34 -0500

    Just kidding about that last push, now Contact.php is ready for public use.

--- public_html/Contact.php
+++ public_html/Contact.php
@@ -178,7 +178,7 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
             <div style="height: 75px"></div>
   	<div class="container">
   		<div class="row">
-  			<div class="col-md-8 col-md-offset-2">
+  			<div class="col-md-6 col-md-offset-3">
   				<h1 class="page-header text-center">Contact Form</h1>
 				<form class="form-horizontal" role="form" method="post" action="Contact.php">
 					<div class="form-group">
