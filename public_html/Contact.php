dcfb7b35a27d89b645d95540b14db497951da3ed
Ian J. Nessler <ian-nessler@uiowa.edu>
2015-07-16 15:26:30 -0500

    Trying a new fix to the contact footer.

--- public_html/Contact.php
+++ public_html/Contact.php
@@ -224,12 +224,12 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
         <div class="push" >
             <hr style="border-width: 2px;  bottom: 0px; border-color: white !important">
             <div class='row container-fluid' style="background-color:#1b1b1b; margin-bottom: auto">
-            <div class="col-md-4" style="margin-top: 40px; background-color:#1b1b1b;">
+            <div class="col-md-10" style="margin-top: 40px; background-color:#1b1b1b;">
                 <font style="color:white;">
                     &copy; University of Iowa Biochemistry Department 2014
                 </font>
             </div>
-                <div class='col-md-4'></div>
+                <div class='col-md-2'></div>
             <div class="col-md-2" style="margin-right:-100px; background-color:#1b1b1b;">
                 <img src="img2/Logo2.png" alt="Smiley face" height="100" width="100" style="background-color: ">
             </div>
